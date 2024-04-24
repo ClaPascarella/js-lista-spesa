3b0700828ce5174c0a1b185bbc2d0b5f1198e621
ClaPascarella <claudiopascarella1996@gmail.com>
2024-04-24 17:22:39 +0200

    stampo a schermo  la lista della spesa nel ciclo for con innerHTML nel ciclo while con append

--- main.js
+++ main.js
@@ -10,21 +10,28 @@ const listaElement = document.querySelector("ul")
 
 //Creo ciclo//
 for (let i=0; i<listaSpesa.length; i++) {
+    
     const element =listaSpesa[i];
     console.log (i, element)
-
-   
+    listaElement.innerHTML += `<li>${element}</li>`;
 }
 
-//MILESTONE 2://
+//MILESTONE 2: ciclo while//
 
 let i = 0;
-while (i< listaElement.length) {
+while (i< listaSpesa.length) {
+
     const element=listaSpesa[i];
     console.log(i,element);
     i++;
+
+    
+    const liElement = document.createElement("li");
+  	liElement.innerText = element;
+  	listaElement.append(liElement);
+   
+}
+ console.log (i);
     
    
 
-}
-console.log (i,);
